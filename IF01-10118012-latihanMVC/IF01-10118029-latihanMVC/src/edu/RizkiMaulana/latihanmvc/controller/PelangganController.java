f4a0b3b2223dddca4d4c1a32aaf91134dfc892af
MochRizkiMaulanaN <55683135+MochRizkiMaulanaN@users.noreply.github.com>
2020-01-08 00:41:49 +0700

    Add files via upload

---
+++ IF01-10118012-latihanMVC/IF01-10118029-latihanMVC/src/edu/RizkiMaulana/latihanmvc/controller/PelangganController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.RizkiMaulana.latihanmvc.controller;
+
+import edu.RizkiMaulana.latihanmvc.model.PelangganModel;
+import edu.RizkiMaulana.latihanmvc.view.PlangganView;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ *
+ * NIM   : 10118012
+ * Nama  : Moch Rizki Maulana N
+ * Kelas : IF-1
+ */
+public class PelangganController {
+    
+   private PelangganModel model;
+
+    public void setModel(PelangganModel model) {
+        this.model = model;
+    }
+   
+   
+   
+   public void resetForm(PlangganView view){
+       String nama = view.getTextNama().getText();
+       String email = view.getTextEmail().getText();
+       String notelepon = view.getTextNoTelpon().getText();
+       
+       if (nama.equals("") && email.equals("") && notelepon.equals("")) {
+           
+       }else{
+       
+       model.resetForm();
+       }
+   }
+   
+   public void simpanForm(PlangganView view){
+       String nama = view.getTextNama().getText();
+       String email = view.getTextEmail().getText();
+       String notelepon = view.getTextNoTelpon().getText();
+       
+       if (nama.trim().equals("")) {
+           JOptionPane.showMessageDialog(view, "Nama Masih Kosong");
+           
+       }else if(email.trim().equals("")) {
+           JOptionPane.showMessageDialog(view, "Email Masih Kosong");
+            
+       }else if(notelepon.trim().equals("")) {
+           JOptionPane.showMessageDialog(view, "No Telepon Masih Kosong");
+             
+       }else{
+           model.simpanForm();
+       }
+   }
+}
---
+++ IF01-10118012-latihanMVC/IF01-10118029-latihanMVC/src/edu/RizkiMaulana/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.RizkiMaulana.latihanmvc.event;
+
+import edu.RizkiMaulana.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ *
+ * NIM   : 10118012
+ * Nama  : Moch Rizki Maulana N
+ * Kelas : IF-1
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
